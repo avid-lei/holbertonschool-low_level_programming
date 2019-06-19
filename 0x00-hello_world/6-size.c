9f3383e00838d45170bc8dbaadfa8d95da9f258e
lei-diva <you@example.com>
2019-06-19 23:27:55 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,26 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char sChar;
+	int sInt;
+	long int sLongInt;
+	long long sLongLong;
+	float sFloat;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(sChar));
+	printf("Size of an int: %d byte(s)\n", sizeof(sInt));
+	printf("Size of a long int: %d byte(s)\n", sizeof(sLongInt));
+	printf("Size of a long long int: %d byte(s)", sizeof(sLongLong));
+	printf("Size of a float: %d byte(s)", sizeof(sFloat));
+
+
+
+	return (0);
+}
