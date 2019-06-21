85c4f4333267fb530d0bf33e46c86e80d8fc9c91
lei-diva <you@example.com>
2019-06-21 04:02:08 +0000

    Advance

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,38 +8,45 @@
 
 int main(void)
 {
+
 int number1;
 int number2;
-int number0;
 int number3;
+int number4;
 
-for (number3 = '0'; number3 <= '9'; number3++)
+for (number4 = '0'; number4 <= '9'; number4++)
 {
-	for (number0 = '0'; number0 <= '9'; number0++)
+	for (number3 = '0'; number3 <= '9'; number3++)
 	{
-		for (number1 = number3; number1 <= '9'; number1++)
+
+		for (number2 = '0'; number2 <= '9'; number2++)
 		{
 
-			for (number2 = number0 + 1; number2 <= '9'; number2++)
+			for (number1 = '0'; number1 <= '9'; number1++)
 			{
-			putchar(number3);
-			putchar(number0);
-			putchar(' ');
-			putchar(number1);
-			putchar(number2);
 
-				if (number3 < '9' || number0 < '8' || number1 < '9' || number2 < '9')
+				if (!(number2 < number4) && !(number1 < number3) && !(number2 == number4 && number1 == number3))
 				{
-				putchar(',');
-				putchar(' ');
+					putchar(number4);
+					putchar(number3);
+					putchar(' ');
+					putchar(number2);
+					putchar(number1);
+
+				if (!(number4 == '9') || !(number3 == '8') || !(number2 == '9') || !(number1 == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
 				}
 			}
 		}
 	}
 }
-	putchar('\n');
+
+		putchar('\n');
+
 
 	return (0);
-
-
 }
