d2f9b5f13518dab8a832e30227bf48924fe19490
lei-diva <you@example.com>
2019-06-21 05:37:19 +0000

    Advance

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-int number1, number2, number3, number4;
 
-for (number4 = '0'; number4 <= '9'; number4++)
+int pair1, pair2;
+
+for (pair1 = 0; pair1 <= 99; pair1++)
 {
-	for (number3 = '0'; number3 <= '9'; number3++)
+	for (pair2 = pair1 + 1; pair2 <= 99; pair2++)
 	{
-		for (number2 = '0'; number2 <= '9'; number2++)
+		int num1 = (pair1 / 10) + 48;
+		int num2 = (pair1 % 10) + 48;
+		int num3 = (pair2 / 10) + 48;
+		int num4 = (pair2 % 10) + 48;
+
+
+		putchar(num1);
+		putchar(num2);
+		putchar(' ');
+		putchar(num3);
+		putchar(num4);
+
+		if (!(num1 == '9') || !(num2 == '8') || !(num3 == '9') || !(num4 == '9'))
 		{
-			for (number1 = '0'; number1 <= '9'; number1++)
-			{
-				if (!(number2 < number4) && !(number1 < number3) &&
-						!(number2 == number4 && number1 == number3))
-				{
-					putchar(number4);
-					putchar(number3);
-					putchar(' ');
-					putchar(number2);
-					putchar(number1);
-					if (!(number4 == '9') || !(number3 == '8') ||
-							!(number2 == '9') || !(number1 == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}	}
-	}}
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+}
 	putchar('\n');
 	return (0);
 }
