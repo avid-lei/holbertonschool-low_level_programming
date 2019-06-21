4fcacb9bfdcda43dc495f62d951c83744ad2e989
lei-diva <you@example.com>
2019-06-21 04:12:57 +0000

    Advance

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,52 +1,38 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-
-int number1;
-int number2;
-int number3;
-int number4;
+int number1, number2, number3, number4;
 
 for (number4 = '0'; number4 <= '9'; number4++)
 {
 	for (number3 = '0'; number3 <= '9'; number3++)
 	{
-
 		for (number2 = '0'; number2 <= '9'; number2++)
 		{
-
 			for (number1 = '0'; number1 <= '9'; number1++)
 			{
-
-				if (!(number2 < number4) && !(number1 < number3) && !(number2 == number4 && number1 == number3))
+				if (!(number2 < number4) && !(number1 < number3) &&
+						!(number2 == number4 && number1 == number3))
 				{
 					putchar(number4);
 					putchar(number3);
 					putchar(' ');
 					putchar(number2);
 					putchar(number1);
-
-				if (!(number4 == '9') || !(number3 == '8') || !(number2 == '9') || !(number1 == '9'))
+					if (!(number4 == '9') || !(number3 == '8') ||
+							!(number2 == '9') || !(number1 == '9'))
 					{
 						putchar(',');
 						putchar(' ');
 					}
-
 				}
-			}
-		}
-	}
-}
-
-		putchar('\n');
-
-
+			}	}
+	}}
+	putchar('\n');
 	return (0);
 }
