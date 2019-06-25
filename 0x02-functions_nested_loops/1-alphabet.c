62927c90252004c05f2104f900851c8a3dc643cb
lei-diva <you@example.com>
2019-06-25 20:08:12 +0000

    0-holberton.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
+* print alphabet - Entry point
+* 
 * Return: Always 0 (Success)
 */
 
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	putchar('\n');
-
-	return (0);
-
-}
 
 void print_alphabet(void)
 {
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* is lower - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+char alower;
+	
+	for (alower = 'a'; alower <= 'z'; alower++)
+		if (c == alower)
+		{
+			return (1); 
+		}
+		else 
+		{ 
+			continue; 
+		}
+return (0);
+}
