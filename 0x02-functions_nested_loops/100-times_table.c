8307dc7b6703507a2b2f0bc1e72aea6e4a62c335
lei-diva <you@example.com>
2019-06-26 06:24:12 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,54 @@
 #include "holberton.h"
-
 /**
-* times_table - prints times table up to 9
-*
+* print_times_table - prints times table up to 9
+*@n: int
 *Return: Always 0
 */
-
-void times_table(int n)
+void print_times_table(int n)
 {
-int factor1, factor2, product, product1, product10;
-
+int factor1, factor2, product, product1, product10, product100;
+if (!(n > 15 || n < 0))
+{
 for (factor1 = 0; factor1 <= n; factor1++)
 {
 	for (factor2 = 0; factor2 <= n; factor2++)
 	{
-		product = factor1 * factor2;
+	product = factor1 * factor2;
+		if (product >= 100)
+		{
+			_putchar(',');
+			_putchar(' ');
 
-		if (product > n)
+		} else if (product >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		} else
 		{
 			if (factor2 != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
-
-			product10 = product / 10;
-			product1 = product % 10;
-			_putchar(product10 + '0');
-			_putchar(product1 + '0');
-
-		}
-		else
-		{
-			if (factor2 != 0)
-			{
-				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-			}
-
-
-			_putchar(product + '0');
-
-		}
 	}
-	_putchar('\n');
+		}
+
+		product100 = product / 100;
+		product10 = (product / 10) % 10;
+		product1 = product % 10;
+
+		if (product100 != 0)
+		{
+			_putchar(product100 + 48);
+		}
+		if (product100 != 0 || product10 != 0)
+		{
+			_putchar(product10 + '0');
+		}
+			_putchar(product1 + '0');
+	} _putchar('\n');
+}
 }
 }
