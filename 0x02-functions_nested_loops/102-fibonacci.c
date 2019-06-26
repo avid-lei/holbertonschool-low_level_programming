44f8463b88bb9c10bf06c042c0dad9e64b0ffbf8
lei-diva <you@example.com>
2019-06-26 07:07:29 +0000

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,18 @@ int main(void)
 {
 int x;
 int sum;
-int num1 =1;
-int num2 =2;
+int num1 = 1;
+int num2 = 2;
 
 printf("%d, ", num1);
 printf("%d, ", num2);
 for (x = 0; x <= 48; x++)
 {
 	sum = num1 + num2;
-	num1 = num2; 
+	num1 = num2;
 	num2 = sum;
 	printf("%d, ", sum);
 }
-
+	printf("\n");
 return (0);
 }
