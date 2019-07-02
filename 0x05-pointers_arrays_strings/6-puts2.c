96ab94e776bfa0c63fa8d07c75871502e52aa829
lei-diva <you@example.com>
2019-07-02 19:03:27 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * puts2 - print every other character
+ * @str: char
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}	
+	_putchar('\n');
+}
+
