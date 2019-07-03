9aa2b2688b8532d7ec39e785dc68f82f3247edcb
lei-diva <you@example.com>
2019-07-03 06:53:41 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_atoi - a to i
+ *@s:char
+ *Return: int
+ */
+
+int _atoi(char *s)
+{
+	int negcounter = 0;
+	int number;
+	while (*s)
+	{
+		if ( *s == '-')
+		{
+			negcounter++;
+			s++;
+		}
+		else if ( *s >= '0' && *s <= '9')
+		{
+			number = *s - '0';
+			s++;
+			while (*s >= '0' && *s <= '9')
+			{
+				number = (number * 10) + (*s - '0');
+				s++;
+			}
+			if (negcounter % 2 == 1)
+				return (number * (-1));
+			else
+				return(number);
+
+		}
+		else
+			s++;
+
+
+	}
+	return (0);
+
+}
