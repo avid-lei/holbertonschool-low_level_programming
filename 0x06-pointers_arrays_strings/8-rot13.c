9da596b0f7eb5b9d9a1f9f4689da4b1fdab0dac7
lei-diva <you@example.com>
2019-07-08 07:01:54 +0000

    8-rot13.c

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+  *rot13 - encodes a string using rot13
+  *@s:char
+  *Return: char*
+  */
+
+char *rot13(char *s)
+{
+
+char *t = s;
+char *in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int x;
+
+while (*t)
+{
+	for (x = 0; *(in + x) != '\0' ; x++)
+	{
+		if (*t == *(in + x))
+		{
+			*t = *(out + x);
+
+
+		}
+
+	}
+	t++;
+}
+
+
+
+return (s);
+}
