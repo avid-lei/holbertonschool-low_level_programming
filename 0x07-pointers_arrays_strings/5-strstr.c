0ae7a5d5e607e7fd332423f3ee5bb1bbf73a527e
lei-diva <you@example.com>
2019-07-10 00:53:07 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ *_strstr - locates a substring
+ * @haystack:char
+ * @needle:char
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *needlecount = needle;
+	int i;
+	int length = 0;
+	int count = 0;
+
+while (*needlecount)
+{
+length++;
+needlecount++;
+}
+
+	while (*haystack)
+	{
+		if (*haystack == needle[0])
+		{
+			count++;
+			haystack++;
+			for (i = 1; needle[i] != '\0'; i++)
+			{
+				if (*haystack == needle[i])
+				{
+					count++;
+					if (count == length)
+					{
+						return (haystack - (count - 1));
+					}
+					else
+					{
+						haystack++;
+					}
+				}
+			}
+		}
+
+			haystack++;
+	}
+return (haystack);
+}
