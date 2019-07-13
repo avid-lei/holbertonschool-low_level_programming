42b99bb543a024d06d8d898d34d2ca3742c33123
lei-diva <you@example.com>
2019-07-13 18:21:38 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,45 +3,45 @@
 #include <stdio.h>
 
 /**
-  *alloc_grid - returns a pointer to 2D array
-  *@width:int
-  *@height:int
-  *Return:int **
-  */
+ *alloc_grid - returns a pointer to 2D array
+ *@width:int
+ *@height:int
+ *Return:int **
+ */
 
 int **alloc_grid(int width, int height)
 {
-int **ar;
-int i, j, x, y;
+	int **ar;
+	int i, j, x, y;
 
 
-if (width <= 0 || height <= 0)
-	return (NULL);
-
-ar = malloc(height * sizeof(int));
-
-if (ar == NULL)
-	return (NULL);
-
-for (i = 0; i < height; i++)
-{
-	ar[i] = malloc(width * sizeof(int));
-
-	if (ar[i] == NULL)
-	{
-		for (x = 0; x < i; x++)
-			free(ar[x]);
-
-		free(ar);
-
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	}
-}
 
-for (y = 0; y < height; y++)
-{
-	for (j = 0; j < width; j++)
-		ar[y][j] = 0;
-}
-return (ar);
+	ar = malloc(height * sizeof(int*));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ar[i] = malloc(width * sizeof(int));
+
+		if (ar[i] == NULL)
+		{
+			for (x = 0; x < i; x++)
+				free(ar[x]);
+
+			free(ar);
+
+			return (NULL);
+		}
+	}
+
+	for (y = 0; y < height; y++)
+	{
+		for (j = 0; j < width; j++)
+			ar[y][j] = 0;
+	}
+	return (ar);
 }
