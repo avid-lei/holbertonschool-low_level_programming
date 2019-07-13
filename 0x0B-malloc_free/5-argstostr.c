f1ed2a86e2033ac1992ae662081057015d8214a2
lei-diva <you@example.com>
2019-07-13 20:43:02 +0000

    5-argstostr.c

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+  *argstostr- concats all the arguments
+  *@ac:int
+  *@av:char
+  *Return: char
+  */
+
+char *argstostr(int ac, char **av)
+{
+
+char *ar;
+int i, j, x = 0;
+int count = 0;
+
+if (ac == 0 || av == NULL)
+	return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+	{
+		count++;
+	}
+	count++;
+}
+
+ar = malloc(count * sizeof(char));
+
+if (ar == NULL)
+{
+	free(ar);
+	return (NULL);
+
+}
+for (x = 0; x < count;)
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, x++)
+			ar[x] = av[i][j];
+
+
+		ar[x] = '\n';
+		x++;
+	}
+return (ar);
+}
