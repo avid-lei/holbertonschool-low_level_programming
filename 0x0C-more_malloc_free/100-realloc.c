7a8c19b2eb9530a26d6fd99b21cbc49c3ebd63fd
lei-diva <you@example.com>
2019-07-14 10:25:36 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,8 +23,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ar);
 	}
 	if (new_size == 0 && ptr != '\0')
+	{
+		free(ptr);
 		return (NULL);
-
+	}
 	if (new_size > old_size)
 	{
 		ar = malloc(new_size * sizeof(char));
