22187d857ff4b855ed82b6a690dfcda0bf397684
lei-diva <you@example.com>
2019-07-14 08:15:07 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ar;
+	int *ar;
+	unsigned int x;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ar = calloc(nmemb, size);
+	ar = malloc(nmemb * size);
 
 	if (ar == NULL)
 		return (NULL);
+
+	for (x = 0; x < nmemb; x++)
+		ar[x] = 0;
 
 	return (ar);
 
