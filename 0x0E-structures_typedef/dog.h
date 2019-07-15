7741fa50e76670f7ec1800e99586ec129d1fcc3e
lei-diva <you@example.com>
2019-07-15 09:35:32 +0000

    set up and 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - struct for dog
+  *@name: char
+  *@age: int
+  *@owner: char
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
