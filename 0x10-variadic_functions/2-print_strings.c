cc9701aa0d54ffde2d6be1d1e4555ce700c39b81
lei-diva <you@example.com>
2019-07-16 09:20:46 +0000

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", separator);
 	}
 
-	printf("%s", va_arg(valist,char *));
+	printf("%s", va_arg(valist, char *));
 	printf("\n");
 
 
