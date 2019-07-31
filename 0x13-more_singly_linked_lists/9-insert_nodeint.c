eda17a02f586c4c1c03d61c07126a07fd8609b9e
lei-diva <you@example.com>
2019-07-31 22:19:07 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,20 +20,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	node->n = n;
 
-	if (((!(*head)) || (!(head))) && idx > 0)
+	if (((!*head) || !head) && idx != 0)
 		return (NULL);
 
 
-	if (((!(*head)) || (!(head))) && idx == 0)
+	if (((!*head) || (!head)) && idx == 0)
 	{
-		*head = new;
+		*head = node;
 		return (node);
 	}
 
 	if (idx == 0)
 	{
 		node->next = *head;
-		*head = new;
+		*head = node;
 		return (node);
 	}
 
