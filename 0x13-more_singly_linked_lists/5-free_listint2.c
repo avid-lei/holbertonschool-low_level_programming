3078462cf9c29df778ddaea3dcd7a92643c2685d
lei-diva <you@example.com>
2019-07-31 18:10:49 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,10 @@ void free_listint2(listint_t **head)
 
 	listint_t *new;
 
-	if (!*head)
-		return;
-
 	if (!head)
 		return;
 
-	while (*head)
+	while ((*head) != NULL)
 	{
 		new = *head;
 		(*head) = (*head)->next;
