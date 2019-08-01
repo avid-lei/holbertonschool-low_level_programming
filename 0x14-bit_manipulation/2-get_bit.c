01d6423b2f798aa5c535e7f323a25f89302dd7d8
lei-diva <you@example.com>
2019-08-01 03:18:46 +0000

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,3 +3,32 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @n:unsigned long int
+ * @index: unsigned int
+ * Return: int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int count;
+unsigned int icopy = n;
+
+while (icopy)
+{
+	count++;
+	icopy = icopy >> 1;
+
+}
+
+if (index > count)
+	return (-1);
+
+if ((n >> index) & 1)
+	return (1);
+
+else
+	return (0);
+
+
+
+
+}
