f164a43dfc2019874175ef410a43fc22f8e7b06a
lei-diva <you@example.com>
2019-08-01 06:35:59 +0000

    100-rese ve

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * reverse_listint - reverse listint
+ * @head: listint_t
+ * Return: listint_t
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p = *head;
+	listint_t *temp;
+
+
+	if (!(*head) || !head)
+		return (NULL);
+
+	temp = (*head)->next;
+	*head = temp;
+	p->next = NULL;
+
+
+	while ((*head)->next)
+	{
+		temp = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		(*head) = temp;
+	}
+
+	(*head)->next = p;
+
+	return (*head);
+
+}
