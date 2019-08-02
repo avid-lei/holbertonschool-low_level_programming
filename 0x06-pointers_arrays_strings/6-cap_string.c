cf23ab86337cab6b570bb3f447e5e858215ba48f
lei-diva <you@example.com>
2019-08-02 10:18:59 +0000

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,12 @@ char *cap_string(char *s)
 	char *check = ",;.!?\"(){} \t\n";
 	int x;
 
+	if (*t >= 'a' && *t <= 'z')
+		*t = *t - 32;
+
 	while (*t)
 	{
+
 		for (x = 0; check[x] != '\0'; x++)
 		{
 			if (*t == check[x] && *(t + 1) >= 'a' && *(t + 1) <= 'z')
