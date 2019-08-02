eec7b7b5a1bc59a151a9c82ef370b55257bdfe5a
lei-diva <you@example.com>
2019-08-02 17:04:45 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,14 +20,13 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-
-	if (text_content == NULL)
-		return (1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
 
 	x = write(fd, text_content, len);
 	close(fd);
