822fc9c574193fc71747c2804757fbce4d8d5e46
lei-diva <you@example.com>
2019-08-02 04:55:05 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,11 @@ size_t print_list(const list_t *h)
 	if (!h)
 		return (0);
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (0);
-	}
 	for (i = 0; h; i++)
 	{
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
