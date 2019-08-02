8f96b95eec295c3d8f986d49629ff475a0fcd71e
lei-diva <you@example.com>
2019-08-02 10:12:29 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,39 +9,21 @@ char *cap_string(char *s)
 {
 	char *t = s;
 	char *check = ",;.!?\"(){} \t\n";
-	int count = 0;
 	int x;
 
 	while (*t)
 	{
-
 		for (x = 0; check[x] != '\0'; x++)
-
-			if (*t == check[x])
+		{
+			if (*t == check[x] && *(t + 1) >= 'a' && *(t + 1) <= 'z')
 			{
-				count = 0;
+				t++;
+				*t = *t - 32;
 				break;
 			}
+		}
 
-		if (*t >= 'a' && *t <= 'z')
-			count++;
-
-		else if (*t >= 'A' && *t <= 'Z')
-			count++;
-
-		else if (*t >= '0' && *t <= '9')
-			count++;
-
-
-		if (count == 1 && (*t >= 'a' && *t <= 'z'))
-			*t = *t - 32;
-
-		t++;
-
-
+	t++;
 	}
-
-
-
 	return (s);
 }
