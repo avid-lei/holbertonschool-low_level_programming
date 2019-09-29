0f224236901a4cc8c608e4de7fcd082b816122c6
lei-diva <you@example.com>
2019-09-29 03:56:11 +0000

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,8 +1,10 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_print - print the hash table
  * @ht: the hash table
  */
+
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, x;
