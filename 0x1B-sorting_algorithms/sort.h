f3ec578bb15043da07b94788ca8cd25d6c0e8e91
lei-diva <you@example.com>
2019-10-01 18:58:33 +0000

    again

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -21,6 +21,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
+
+void caller(int *array, int start, int end, size_t size);
+int partition(int *array, int start, int end, size_t size);
+void swap(int *low, int *high);
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
