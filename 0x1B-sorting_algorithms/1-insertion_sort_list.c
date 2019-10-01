a8b166d666186b37a7af71664b9d6ae33017aa1f
lei-diva <you@example.com>
2019-10-01 13:05:33 +0000

    rest

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort list via insertion
+ * @list: listint_t
+ * Return: always 0
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *one = *list;
+	listint_t *two = one->next;
+	listint_t *marker = *list;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	while (two)
+	{
+		marker = two;
+		if (two->n < one->n)
+		{
+			marker = one;
+			while (two->n < one->n)
+			{
+				two->prev = one->prev;
+				one->next = two->next;
+				two->next = one;
+				one->prev = two;
+				if (one->next)
+					one->next->prev = one;
+				if (two->prev)
+					two->prev->next = two;
+				else
+				{
+					*list = two;
+					print_list(*list);
+					break;
+				}
+				print_list(*list);
+				one = two->prev;
+			}
+			one = marker;
+			two = marker->next;
+		}
+		else
+		{
+			one = marker;
+			two = marker->next;
+		}
+	}
+}
---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting via selection method
+ * @array: int
+ * @size: size_t
+ * Return: always 0
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t cycle, pos;
+	int low, temp;
+
+	for (cycle = 0; cycle < size - 1; cycle++)
+	{
+
+		for (pos = cycle, low = pos; pos < size; pos++)
+		{
+			if (array[pos] < array[low])
+			{
+				low = pos;
+			}
+
+		}
+		if (array[cycle] != array[low])
+		{
+			temp = array[cycle];
+			array[cycle] = array[low];
+			array[low] = temp;
+			print_array(array, size);
+		}
+	}
+}
