b3a8b3a8cf611838aeebfa03de3120fa25c000d7
lei-diva <divaaleii@gmail.com>
2019-12-04 22:42:56 +0000

    1-binary_tree_insert_left.c

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,40 +1,58 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - insert left node
+ * @parent: binary_tree_t
+ * @value: int
+ * Return: binary_tree_t
+ */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
-    binary_tree_t *temp = parent;
-    if (!node)
-        return (NULL);
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *temp;
 
-    if (!parent)
-        return (NULL);
+	if (!node)
+		return (NULL);
 
-    node->n = value;
-    node->right = NULL;
+	if (!parent)
+		return (NULL);
 
-    re_left(temp, node);
-    return (node);
+	temp = parent;
+
+	node->n = value;
+	node->right = NULL;
+
+	re_left(temp, node);
+	return (node);
 
 }
 
+/**
+ * re_left - recursive solution
+ * @t: binary_tree_t
+ * @node: binary_tree_t
+ * Return: Always 0
+ */
+
 void re_left(binary_tree_t *t, binary_tree_t *node)
 {
-    if (!(t->left))
-    {
-        node->parent = t;
-        node->left = NULL;
-        t->left = node;
-        return;
-    }
-    else if (t->left->n < node->n)
-    {
-        node->parent = t;
-        node->left = t->left;
-        t->left->parent = node;
-        t->left = node;
-        return;
-    }
-    else
-        re_left(t->left, node);
+	if (!(t->left))
+	{
+		node->parent = t;
+		node->left = NULL;
+		t->left = node;
+		return;
+	}
+	else if (t->left->n < node->n)
+	{
+		node->parent = t;
+		node->left = t->left;
+		t->left->parent = node;
+		t->left = node;
+		return;
+	}
+	else
+		re_left(t->left, node);
+
 }
