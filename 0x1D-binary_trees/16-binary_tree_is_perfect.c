0921ec6aed0421dd4d86324e16dd0d5c96f324c4
lei-diva <divaaleii@gmail.com>
2019-12-05 03:11:30 +0000

    16-binary_tree_is_perfect.c

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,22 +1,25 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_is_perfect - check if tree is perfect
+ * @tree: binary_tree_t
+ * Return: int
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int check_l, check_r;
+	int check_l, check_r;
 
-    if (!tree)
-        return (0);
-    
-    else
-    {
-        check_l = 1 + binary_tree_is_perfect(tree->left);
-        check_r = 1 + binary_tree_is_perfect(tree->right);
+	if (!tree)
+		return (0);
 
-        if (check_l != check_r)
-            return (0);
-        else
-            return (1);
-    
-    }
+
+	check_l = 1 + binary_tree_is_perfect(tree->left);
+	check_r = 1 + binary_tree_is_perfect(tree->right);
+
+	if (check_l != check_r)
+		return (0);
+	else
+		return (1);
+
+
 }
