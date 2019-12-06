77d6cf3878d08cc009588d27e07b0cf0cc6dbc9b
lei-diva <divaaleii@gmail.com>
2019-12-06 06:59:05 +0000

    14-binary_tree_balance.c

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -5,42 +5,40 @@
  * @tree: binary_tree_t
  * Return: int
  */
+int re_balance(binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t height_l, height_r;
+	int height_l, height_r;
 
 	if (!tree)
 		return (0);
 
-	height_r = re_height_r(tree);
-	height_l = re_height_l(tree);
+	height_r = re_balance(tree->right);
+	height_l = re_balance(tree->left);
+
 	return (height_l - height_r);
 
 }
 
 /**
- * re_height_l - calculate left height
+ * re_balance - recursive to calculate height
  * @tree: binary_tree_t
- * Return: height
- */
-int re_height_l(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	else
-		return (1 + re_height_l(tree->left));
-}
-
-/**
- * re_height_r - calculate right height
- * @tree: binary_tree_t
- * Return: height
+ * Return: int
  */
 
-int re_height_r(const binary_tree_t *tree)
+int re_balance(binary_tree_t *tree)
 {
+	int l, r;
+
 	if (!tree)
 		return (0);
+
+	l = 1 + re_balance(tree->left);
+	r = 1 + re_balance(tree->right);
+
+	if (l >= r)
+		return (l);
 	else
-		return (1 + re_height_r(tree->right));
+		return (r);
+
 }
