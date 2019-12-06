1291530d0834d9f33387a08cb675a5745897c48d
lei-diva <divaaleii@gmail.com>
2019-12-06 10:28:17 +0000

    102-binary_tree_is_complete.c

---
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -0,0 +1,69 @@
+#include "binary_trees.h"
+
+
+int re_complete(const binary_tree_t *tree, int i, int flag);
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	int i, j, flag = 0;
+	int height;
+
+	height = re_levelheight(tree);
+
+	if (!tree)
+		return (0);
+
+	for (i = 1; i < height; i++)
+	{
+		if (i == height - 1)
+			flag = 1;
+
+		j = re_complete(tree, i, flag);
+		if (j == 0)
+			return (0);
+
+	}
+
+	return (1);
+}
+
+
+int re_complete(const binary_tree_t *tree, int i, int flag)
+{
+	int r, l;
+
+	if (!tree)
+		return (1);
+
+	if (i == 1 && flag == 0)
+	{
+		if (!tree->left || !tree->right)
+		{
+			return (0);
+		}
+
+		return (1);
+	}
+
+
+	if (i == 1 && flag == 1)
+	{
+		if (tree->right && !tree->left)
+			return (0);
+
+		return (1);
+	}
+
+
+	l = re_complete(tree->left, i - 1, flag);
+
+	if (l == 0)
+		return (0);
+
+	r = re_complete(tree->right, i - 1, flag);
+
+	if (r == 0)
+		return (0);
+
+	else
+		return (1);
+}
