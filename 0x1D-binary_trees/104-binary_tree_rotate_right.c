87de4e125f32fef8fcf235e70225a68e66c452a5
lei-diva <divaaleii@gmail.com>
2019-12-06 12:13:28 +0000

    104-binary_tree_rotate_right.c

--- 0x1D-binary_trees/104-binary_tree_rotate_right.c
+++ 0x1D-binary_trees/104-binary_tree_rotate_right.c
@@ -20,8 +20,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 
 		tree->left->parent = tree->left;
 	}
-	else
-		tree->left = NULL;
+
 
 	tree->parent->right = tree;
 	tree->parent->parent = NULL;
