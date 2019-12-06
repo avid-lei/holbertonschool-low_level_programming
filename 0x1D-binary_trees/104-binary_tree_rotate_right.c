9fb9ddf9adf25714d2c87d4796a65f12d13666be
lei-diva <divaaleii@gmail.com>
2019-12-06 11:52:01 +0000

    104-binary_tree_rotate_right.c

---
+++ 0x1D-binary_trees/104-binary_tree_rotate_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - rotate tree one node to the left
+ * @tree: binary_tree_t
+ * Return: binary_tree_t
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+
+	if (!tree || !tree->left)
+		return (NULL);
+
+	tree->parent = tree->left;
+
+	if (tree->left->right)
+		tree->left = tree->parent->right;
+
+	else
+		tree->left = NULL;
+
+	tree->parent->right = tree;
+	tree->parent->parent = NULL;
+	return (tree->parent);
+
+}
