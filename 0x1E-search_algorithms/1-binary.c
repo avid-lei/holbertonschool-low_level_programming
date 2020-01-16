48ba5c98dadc82e8d306bd545c3ffb237646d0b3
lei-diva <divaaleii@gmail.com>
2020-01-16 00:38:51 +0000

    1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+
+/**
+ * binary_search - search algo via binary
+ * @array: int
+ * @size: size_t
+ * @value: int
+ * Return: int
+ */
+
+
+int binary_search(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+
+	return (binary_help(array, 0, size - 1, value));
+
+}
+
+/**
+ * binary_help - helper function for binary search
+ * @array: int
+ * @start: int
+ * @end: int
+ * @value: int
+ * Return: int
+ */
+
+int binary_help(int *array, int start, int end, int value)
+{
+	int mid, x;
+
+	printf("Searching in array: ");
+
+	for (x = start; x <= end; x++)
+		printf("%d ", array[x]);
+
+	printf("\n");
+
+
+	if ((end == start) && (value != array[start]))
+		return (-1);
+
+	mid = start + ((end - start) / 2);
+
+	if (value == array[mid])
+		return (mid);
+	if (value < array[mid])
+		return (binary_help(array, start, mid - 1, value));
+	else
+		return (binary_help(array, mid + 1, end, value));
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,7 +4,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int binary_help(int *array, int start, int end, int value);
 
 
 #endif
