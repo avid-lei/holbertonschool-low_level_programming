eb38ee6a112e1d87abef4865e19bab42a03c70aa
lei-diva <divaaleii@gmail.com>
2020-01-16 11:17:16 +0000

    103

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,7 @@ int binary_search(int *array, size_t size, int value);
 int binary_help(int *array, int start, int end, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
 
 #endif
